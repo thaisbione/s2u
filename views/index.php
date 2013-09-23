47884bcedf31f9ceeb44daa3b53a7ed046947684
Thaís Bione <thaisbione@gmail.com>
2013-09-23 16:47:36 -0300

    Cria banner com js

--- views/index.php
+++ views/index.php
@@ -2,9 +2,26 @@
 	include 'default.php';
 ?>
 		<div class="context">
+			    <link rel="stylesheet" href="../css/dark.css" type="text/css" media="screen" />
+			    <link rel="stylesheet" href="../css/nivo-slider.css" type="text/css" media="screen" />
+			    <link rel="stylesheet" href="../css/style.css" type="text/css" media="screen" />
+			    <script type="text/javascript" src="../js/jquery.nivo.slider.js"></script>
 			<div id="inicio">
 				<div class="introducao-texto call">
-					<span class="introducao">Banner</span>
+					<div id="wrapper">
+				        <div class="slider-wrapper theme-dark">
+				            <div id="slider" class="nivoSlider">
+				                <img src="../imagens/1.jpg" data-thumb="../imagens/toystory.jpg" alt="" />
+				                <img src="../imagens/4.jpg" data-thumb="images/up.jpg" alt=""/></a>
+				                <img src="../imagens/2.jpg" data-thumb="../images/walle.jpg" alt="" data-transition="slideInLeft" />
+				                <img src="../imagens/3.jpg" data-thumb="../images/nemo.jpg" alt=""/>
+				            </div>
+				            <div id="htmlcaption" class="nivo-html-caption">
+				                <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>. 
+				            </div>
+				        </div>
+				    </div>
+		
 					<h2>Início</h2>
 					<p> Olá, somos a S2U (Services to You).  Seu portal de serviços. Aqui você irá encontrar as mais diversas mãos de obra para um trabalho de qualidade. A origem deste portal se deu da necessidade de um serviço terceirizado e a falta de recursos para encontrá-lo.</p>
 					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -24,5 +41,11 @@
 			</div>
 		</div>
 	</div>
+    <script type="text/javascript" src="../js/jquery.nivo.slider.pack.js"></script>
+	    <script type="text/javascript">
+	    $(window).load(function() {
+	        $('#slider').nivoSlider();
+	    });
+    </script>
 </body>
 </html>
